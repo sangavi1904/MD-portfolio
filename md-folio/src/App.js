28d512b225901aff2eae07f2defa31a536eb596f
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-01-05 15:51:28 +0530

    adding compo

--- md-folio/src/App.js
+++ md-folio/src/App.js
@@ -2,12 +2,16 @@
 import './Sara.css';
 import Card from './Card'
 import Contact from './Contact'
+import Client from './Client'
+import Car from './Car'
 
 function App() {
   return (
     <div className="App">
      <Card/>
      <Contact/>
+     <Client/>
+     
     </div>
   );
 }
